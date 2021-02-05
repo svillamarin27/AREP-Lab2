09d9ba0c8f6240e83b9d9908b40983669fb1eae1
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-02-04 22:29:34 -0500

    enlace

--- src/main/java/edu/escuelaing/arep/sparkWeb/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/sparkWeb/SparkWebApp.java
@@ -5,7 +5,7 @@ public class SparkWebApp {
 
 	public static void main(String[] args) {
 		port(getPort());
-		get("/hello", (req, res) -> "Hello Heroku");
+		get("/villamarin", (req, res) -> "Hello Heroku");
 	}
 	static int getPort() {
 		 if (System.getenv("PORT") != null) {
