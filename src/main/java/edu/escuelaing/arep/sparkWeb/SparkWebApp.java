bd8d321ca40821a362a49c0eaac6237fc0347538
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-02-05 00:16:35 -0500

    aplicaion terminada

--- src/main/java/edu/escuelaing/arep/sparkWeb/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/sparkWeb/SparkWebApp.java
@@ -1,11 +1,12 @@
 package edu.escuelaing.arep.sparkWeb;
 
 import static spark.Spark.*;
+/**
 public class SparkWebApp {
 
 	public static void main(String[] args) {
 		port(getPort());
-		get("/villamarin", (req, res) -> "Hello Heroku");
+		get("/villamarin", (req, res) -> "Hello Herssoku");
 	}
 	static int getPort() {
 		 if (System.getenv("PORT") != null) {
@@ -14,56 +15,68 @@ public class SparkWebApp {
 		 return 4567; //returns default port if heroku-port isn't set
 		 }
 }
-
-/**
+*/
 import spark.Request;
 import spark.Response;
 import static spark.Spark.*;
+import java.awt.List;
+
+import edu.escuelaing.arep.sparkWeb.Calculos.app.App;
+import edu.escuelaing.arep.sparkWeb.Calculos.util.*;
 
 /**
  * Minimal web app example for Heroku using SparkWeb
  *
- * @author daniel
+ * @author SebastiÃ¡n VillamarÃ­n
  */
-/**
-public class SparkWebApp {
 
-    /**
+
+public class SparkWebApp {
+     /*
      * This main method uses SparkWeb static methods and lambda functions to
      * create a simple Hello World web app. It maps the lambda function to the
-     * /hello relative URL.
+     *hello relative URL.
+    */
      
     public static void main(String[] args) {
         port(getPort());
-        get("/inputdata", (req, res) -> inputDataPage(req, res));
-        get("/results", (req, res) -> resultsPage(req, res));
+        get("/villamarin", (req, res) -> inputDataPage(req, res));
+        get("/resultados", (req, res) -> resultsPage(req, res));
     }
 
     private static String inputDataPage(Request req, Response res) {
-        String pageContent
-                = "<!DOCTYPE html>"
-                + "<html>"
-                + "<body>"
-                + "<h2>HTML Forms</h2>"
-                + "<form action=\"/results\">"
-                + "  First name:<br>"
-                + "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">"
-                + "  <br>"
-                + "  Last name:<br>"
-                + "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">"
-                + "  <br><br>"
-                + "  <input type=\"submit\" value=\"Submit\">"
-                + "</form>"
-                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
-                + "</body>"
-                + "</html>";
-        return pageContent;
+    	String page
+			="<!DOCTYPE html>" 
+			+"<html>"
+			+"<body style=\"background-color:#F8F9C1;\">" 
+			+"<font align=\"center\" color=\"Olive\" face=\"Comic Sans MS,arial\">"
+			+ "<h1>Calculadora para hallar la media y desviación estandar</h1>"
+			+ "<h2>Ingrese los números separados por un punto y coma (;) </h2>" 
+			+ "<form action='/resultados\'>" 
+			+ "<input type=\"text\" name='data'>"
+			+ "<input type=\"submit\" value=\"Calcular\">"  
+			+ "</form>" 
+			+ "</body>"
+			+ "</html>";
+		return page;
     }
 
     private static String resultsPage(Request req, Response res) {
-        return req.queryParams("firstname") + " " +
-                req.queryParams("lastname");
-    }
+		LinkedList<Double> listan=new LinkedList<Double>();
+		String [] valores=req.queryParams("data").split(";");
+		for(String i:valores) {
+			listan.add(Double.parseDouble(i));
+		}
+		String resp = "<!DOCTYPE html>" 
+				+"<html>"
+				+"<body style=\"background-color:#F8F9C1;\">"
+				+"<font align=\"center\" color=\"Olive\" face=\"Comic Sans MS,arial\">"
+				+ "<h2>"+ "El resultado de la media es: " + App.media(listan) + "<h2>"
+				+ "<h2>"+ "El resultado de la desviacion estandar: " + App.desviacion(listan) + "<h2>"
+				+ "</body>"
+				+ "</html>";
+		return resp;
+	}
 
     /**
      * This method reads the default port as specified by the PORT variable in
@@ -71,6 +84,7 @@ public class SparkWebApp {
      *
      * Heroku provides the port automatically so you need this to run the
      * project on Heroku.
+     */
      
     static int getPort() {
         if (System.getenv("PORT") != null) {
@@ -78,6 +92,6 @@ public class SparkWebApp {
         }
         return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
-
 }
-*/
+
+
